17efd766377e11b3d1b35b2ce1274fa0b5608a86
amruta2001 <60223535+Amruta-Totala@users.noreply.github.com>
2022-06-25 23:27:41 +0530

    Add files via upload

--- chart_django_project/settings.py
+++ chart_django_project/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
